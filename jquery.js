ebcfa0d39ba09bcd6c54258fcc111de0515d7daf
Bruno <96424641+brunoeverardo@users.noreply.github.com>
2025-05-17 10:40:58 -0300

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,5 @@
+document.getElementById("meuBotao").addEventListener("click", function() {
+  document.getElementById("mensagem").style.display = "block"; // Mostrar a mensagem
+  // Alterar o texto da mensagem (opcional)
+  document.getElementById("mensagem").textContent = "Abra os olhos para um dia cheio de possibilidades e faça o melhor de cada momento com entusiasmo e coragem.";
+});
